931b123ee063591f9411b48d34c8a1659f876ea1
stypes <juanto213@gmail.com>
2022-01-12 00:20:22 -0300

    index shades

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 export const Header = () => {
     return (
 
-        <div className="container relative ">
-           <div className="px-40">
-           <h1 className="text-gray-100 antialiased text-shadow-xl text-5xl uppercase font-semibold grid mx-64 bottom-20  absolute text-center ">CIRCULO MAGICO 
-           <span className="text-4xl antialiased uppercase italic mt-3 tracking-widest text-red-800">Escuela de Magia e Ilusionismo</span></h1>
-               <picture className="">
-                 <source srcSet="/img/slides/cartas.avif" type="image/avif" />
-                 <source srcSet="/img/slides/cartas.webp" type="image/webp" />
-                 <img loading="lazy" src="/img/slides/cartas.jpg" alt="Escuela de Magia e Ilusionismo"  />
-              </picture>
+        <div className="container header ">
+           <div className="">
+         
+           <h1 className="degradate">
+               CIRCULO MAGICO <br />
+           <span className="subti ">Escuela de Magia e Ilusionismo</span></h1>
+           
+               
             </div>
         </div>
     )
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,20 +1,25 @@
 const Servicios = () => {
     return (
-        <div className="container text-center grid grid-cols-3 ">
+        <div className="container text-center grid lg:grid-cols-3 md:grid-1  serv">
             
                 <div className="order-1">
+               
+                <i class="fas fa-door-closed"></i>
+
                 <h2 className="text-white uppercase text-3xl mb-4">Donde Estamos? </h2> 
                 <p className="text-white">Barrio de Caballito - Felipe Vallese 1387.
                 <br /> WhatsApp al: 11-5263-8510.
                 <br /> Ver Mapa</p>
                 </div>
                 <div className="order-2">
+                <i class="fas fa-hat-wizard"></i>
                 <h2 className="text-white uppercase text-3xl mb-4">Aprender Magia </h2> 
                 <p className="text-white">Las clases de ilusionismo son dictadas por<br /> 
                 Campeones Latinoamericanos especializados en<br />
                 una rama de la magia particular y con un estilo diferente.</p>
                 </div>
                 <div  className="order-3">
+                <i class="fas fa-shopping-cart"></i>
                 <h2 className="text-white uppercase text-3xl mb-4">Botones </h2> 
                 <p className="text-white">Barrio de Caballito - Felipe Vallese 1387.
                 <br /> WhatsApp al: 11-5263-8510.
